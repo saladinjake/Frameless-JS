d86bf4fff7d127d2ff650ddc65726ab44a75db7e
Victor Juwa <juwavictor@gmail.com>
2025-07-19 19:28:36 +0100

    chore: update fix on ts loader or js script loader

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,4 @@ import { routes } from './AppRoutes';
 
 // bootstrap the mini framework engine
 const root = document.getElementById('app');
-console.log(root)
-console.log(routes)
 bootstrapContainers(routes).surge(root);
